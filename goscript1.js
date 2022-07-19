7e008affc34de896dcceafb95bb86e6650484941
neokin77 <103141282+neokin77@users.noreply.github.com>
2022-07-18 23:42:10 -0300

    Create goscript1.js

---
+++ goscript1.js
@@ -0,0 +1,29 @@
+const urlTarget = "https://goanimes.net/";
+
+function handlePopup(url) {
+  window.open(url, "_blank");
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function popEvent(url, time) {
+  localStorage.setItem('popupday', time);
+  handlePopup(url);
+}
+
+document.addEventListener('click', (e) => {
+  const popupStorage = localStorage.getItem('popupday');
+  const timePop = new Date().getTime();
+  const expireTimePop = 1000 * 60 * 60 * 1;
+  console.log(expireTimePop);
+  if (popupStorage === null) {
+    popEvent(urlTarget, timePop);
+  } else {
+    const timeDiff = timePop - popupStorage;
+    if (timeDiff > expireTimePop) {
+      popEvent(urlTarget, timePop);
+    }
+  }
+});
