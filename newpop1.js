1e6f360e33e13b487b29dd3ae2bdbc613063e185
neokin77 <103141282+neokin77@users.noreply.github.com>
2022-09-25 12:27:30 -0300

    Create newpop1.js

---
+++ newpop1.js
@@ -0,0 +1,17 @@
+var tags=document.getElementsByTagName("a");for(i=0;i<tags.length;i++){tags[i].onclick=function(){if((this.getAttribute('href')!='https://gojoanimes.top/')&&(this.getAttribute('href')!='https://gojoanimes.top/')){if(LerCookie('Encaminhamento')!="encaminhado"){openWindow('https://gojoanimes.biz/');}
+GerarCookie('Encaminhamento','encaminhado',3600);};}}
+function openWindow(url=''){var l=screen.availWidth;var t=screen.availHeight;var p='height='+t+',width='+l+',menubar=yes,toolbar=yes,location=yes,scrollbars=yes,status=yes,resizable=yes,left=0,top=0';nw=window.open(url,"nt",p);this.focus();}
+function GerarCookie(strCookie,strValor,lngHoras){var dtmData=new Date();if(lngHoras)
+{dtmData.setTime(dtmData.getTime()+(lngHoras*60*1000));var strExpires="; expires="+dtmData.toGMTString();}
+else
+{var strExpires="";}
+document.cookie=strCookie+"="+strValor+strExpires+"; path=/";}
+function LerCookie(strCookie)
+{var strNomeIgual=strCookie+"=";var arrCookies=document.cookie.split(';');for(var i=0;i<arrCookies.length;i++)
+{var strValorCookie=arrCookies[i];while(strValorCookie.charAt(0)==' ')
+{strValorCookie=strValorCookie.substring(1,strValorCookie.length);}
+if(strValorCookie.indexOf(strNomeIgual)==0)
+{return strValorCookie.substring(strNomeIgual.length,strValorCookie.length);}}
+return null;}
+function ExcluirCookie(strCookie)
+{GerarCookie(strCookie,'',-3600);}
